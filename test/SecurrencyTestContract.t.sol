30f7618176518e61a6fd1d80da33cc4c4de94bd0
Mohammad Salim <jd2474ev@gmail.com>
2023-10-18 22:27:39 -0400

    Task3: Created tests for the 'createAssetDetails' function with a successful contract compile

--- test/SecurrencyTestContract.t.sol
+++ test/SecurrencyTestContract.t.sol
@@ -45,4 +45,30 @@ contract SecurrencyTestContractTest is Test {
         assertTrue(isVerifiedInvestor);
         assertFalse(isUSResident);
     }
+    
+    /// Test for StackOverflow
+    function testStackOverflow() public {
+        StackOverflow.AssetDetails memory details = StackOverflow.AssetDetails({
+            currency: address(0x0987654321098765432109876543210987654321),
+            settlementCurrency: address(0x1123456789012345678901234567890123456789),
+            marketObjectCodeRateReset: keccak256("test"),
+            notionalPrincipal: 1000,
+            nominalInterestRate: 5,
+            accruedInterest: 2,
+            rateMultiplier: 3,
+            contractDealDate: block.timestamp,
+            statusDate: block.timestamp + 1 days,
+            initialExchangeDate: block.timestamp + 2 days,
+            maturityDate: block.timestamp + 365 days,
+            purchaseDate: block.timestamp + 10 days,
+            capitalizationEndDate: block.timestamp + 20 days,
+            cycleAnchorDateOfInterestPayment: block.timestamp + 30 days,
+            cycleAnchorDateOfRateReset: block.timestamp + 40 days,
+            cycleAnchorDateOfScalingIndex: block.timestamp + 50 days,
+            cycleAnchorDateOfFee: block.timestamp + 60 days
+        });
+        stackOverflow.createAssetDetails(details);
+        // Assuming `saveDetailsToStorage` is a mock, there's no state change to check
+        // If state changes were present, we'd use assertions here.
+    }
 }
